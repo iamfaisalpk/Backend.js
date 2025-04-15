5932a672b0f1c6378064faf194519c1d6b13b6d1
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-15 13:17:26 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-const path = require("path")
-const fs =require("fs");
+// const http = require('http');
 
-fs.mkdir(path.join(__dirname,"/api"),{},(err)=>{
-    if(err) throw err
+// const server = http.createServer((req,res)=>{
+//     res.write("hi")
+//     res.end()
+// })
+
+// server.listen("4040")
+
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     if (req.url === '/hj'){
+//         res.write('hloo')
+//         res.end()
+//     }else {
+//         res.end("error")
+//     }
+// })
+
+// server.listen("8080")
+
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.write("hello, task");
+    res.end();
+});
+
+server.listen(3000, ()=>{
+    console.log("server side running")
 })
 
-console.log(fs);
 
-// console.log(path.join(__dirname,"api","index.js"))
+// const fs = require('fs');
+
+// console.log("start");
+
+// fs.readFile('file.text','utf8',(err,data)=>{
+//     if (err)throw err;
+//     console.log("file data:", data)
+// })
+
+// console.log("end")
