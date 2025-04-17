03261686fee375aab253881705ffd5f3c8faa009
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-17 13:53:44 +0530

    first commit

--- index.js
+++ index.js
@@ -20,18 +20,18 @@
 
 // server.listen("8080")
 
-// const http = require("http");
+const http = require("http");
 
-// const server = http.createServer((req,res)=>{
-//     if (req.url === "/"){
-//         res.write("hiii");
-//         res.end()
-//     } else {
-//         res.end("error")
-//     }
-// })
+const server = http.createServer((req,res)=>{
+    if (req.url === '/'){
+        res.write("hiii")
+        res.end()
+    } else {
+        res.end("error")
+    }
+})
 
-// server.listen("5050")
+server.listen("5050")
 
 // const http = require("http");
 
@@ -56,11 +56,11 @@
 
 
 
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    res.write("hiilo");
-    res.end()
-})
+// const server = http.createServer((req,res)=>{
+//     res.write("hiilo");
+//     res.end()
+// })
 
-server.listen("7860");
+// server.listen("7860");
