ab230ab55377e6c637b91d7ef6c26fdd482a9dc4
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-17 13:58:07 +0530

    second commit

--- index.js
+++ index.js
@@ -20,18 +20,18 @@
 
 // server.listen("8080")
 
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    if (req.url === '/'){
-        res.write("hiii")
-        res.end()
-    } else {
-        res.end("error")
-    }
-})
+// const server = http.createServer((req,res)=>{
+//     if (req.url === '/'){
+//         res.write("hiii")
+//         res.end()
+//     } else {
+//         res.end("error")
+//     }
+// })
 
-server.listen("5050")
+// server.listen("5050")
 
 // const http = require("http");
 
@@ -54,13 +54,19 @@ server.listen("5050")
 
 // console.log("end")
 
+const http = require("http");
 
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.write("hlooi");
+        res.end()
+    } else if (req.url === "/home"){
+        res.write("welcoe to home")
+        res.end()
+    } else {
+        res.write("error")
+        res.end()
+    }
+});
 
-// const http = require("http");
-
-// const server = http.createServer((req,res)=>{
-//     res.write("hiilo");
-//     res.end()
-// })
-
-// server.listen("7860");
+server.listen
