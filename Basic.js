e3613e8dea25998eb3b54e9d9d9c51e334d40eaa
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-19 16:53:30 +0530

    foorth commt

--- Basic.js
+++ Basic.js
@@ -1,1 +1,14 @@
-console.log(global);
+const path =  require("path")
+const fs =  require("fs");
+
+const user = "faisal" ; 
+
+// fs.writeFile(path.join(__dirname, "/API", "api.txt"), `UserName : Iam ${user}`,(err)=>{
+//     if(err) throw err;
+// })
+
+
+fs.appendFile(path.join(__dirname, "/API", "api.txt"), `\n UserName : ${user}`, (error)=>{
+    if (error) throw error;
+})
+
