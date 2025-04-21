97083915edacb61acfbbde1e9ee630c1d9add61a
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-21 10:49:12 +0530

    second commit

--- Advnced.js
+++ Advnced.js
@@ -1,8 +1,13 @@
 const http = require("http");
+const fs =  require("fs");
+const path = require("path")
 
 http.createServer((req,rs)=>{
     rs.write("hii welcome");
     console.log(req.url);
+    console.log("Test");
+
+
     
     rs.end();
 }).listen(3006, ()=> console.log("server running"))
