589c9cc3dbd7733806d41ca8b4f23cbc8484a9be
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-25 10:15:20 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 7860;
+
+app.get('/', (req,res)=>{
+    res.send("hoe world kooi");
+})
+
+app.listen(port, ()=>{
+    console.log(`app listening on port : ${port}`);
+    
+})
