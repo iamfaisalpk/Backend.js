65f6df69ece990c86787684572ce850ceb8b37be
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-30 11:21:12 +0530

    second cmmit

--- Routeprms.js
+++ Routeprms.js
@@ -6,9 +6,12 @@ const port = 3080;
 
 
 app.get("/flights/:from-:to", (req,res)=>{
-    res.send(`F`)
+    res.send(`Flights from ${req.params.from} to ${req.params.to}`)
 })
 
+app.get("/place/:state.:dist", (req,res)=>{
+    res.send(`State ${req.params.state} District : ${req.params.dist}`)
+})
 
 // app.get("/product/:category/:brad", (req,res)=>{
 //     // res.send(req.params.category)
