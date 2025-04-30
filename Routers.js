9c3461d5723f63add44a40703d17d2da9f2a4974
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-30 13:53:20 +0530

    fifth commit

---
+++ Routers.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 1212;
+
+app.get('/product', (req,res)=>{
+    res.send('all products')
+})
+
+app.get('/User', (req,res)=>{
+    res.send('User Profile')
+})
+
+app.get('/About', (req,res)=>{
+    res.send('About page')
+})
+
+app.get('/Home page', (req,res)=>{
+    res.send('Welcome to my home')
+})
+
+app.listen(port, ()=>{
+    console.log(`running server ${port}`)
+})
