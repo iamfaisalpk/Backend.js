a3fa47d46fae3f0758caeaf4a84ad6323f32bc0b
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-30 11:33:20 +0530

    third commit

--- filedownloading.js
+++ filedownloading.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
 const port = 7860;
 
-
+app.get('/', (req,res)=>{
+    res.download(path.join(__dirname, "Sample.txt"))
+})
 
 
 app.listen(port, ()=>{
