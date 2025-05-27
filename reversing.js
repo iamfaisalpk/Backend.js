fa50a6429951b856977638413f052762174879ba
iamfaisalpk <iamfaizy123@gmail.com>
2025-05-27 14:42:53 +0530

    second commit

--- reversing.js
+++ reversing.js
@@ -1,11 +1,12 @@
-const reversing = (str)=>{
-    let reversed = '';
+const reversing = (num)=>{
+    let str = num.toString();
 
+    let reversedStr = '';
     for (let i = str.length -1; i >=0; i--){
-        reversed += str[i];
+        reversedStr += str[i];
     }
 
-    return reversed;
+    return parseInt(reversedStr);
 }
 
-console.log(reversing("hello"))
+console.log(reversing(12345))
