7646f6398ea872c14707714eacdd5d000d5cd24f
iamfaisalpk <iamfaizy123@gmail.com>
2025-05-28 13:19:22 +0530

    first comit

--- Server.js
+++ Server.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const useRoutes  = require("./userRoutes");
-const app = express();
-
 
 dotenv.config();
+
+const app =express();
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(()=> console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+    .then (()=> console.log("connected mongoDB"))
+    .catch((err)=>{
+        console.error("connection failed",err.message)
+        process.exit(1)
+    });
 
-    app.use('/api/user',useRoutes)
+app.get("/", (req,res)=>{
+    res.send("Server is running and mongoDB connected")
+});
 
-    const PORT = process.env.PORT || 7860;
+const PORT = process.env.PORT || 7860;
+
 app.listen(PORT, ()=>{
-    console.log(`Server running on http://localhost${PORT}`);
-    
+    console.log(`Server is running port${PORT}`)
 })
--- reversing.js
+++ reversing.js
@@ -1,12 +1,24 @@
-const reversing = (num)=>{
-    let str = num.toString();
+// const reversing = (num)=>{
+//     let str = num.toString();
 
-    let reversedStr = '';
-    for (let i = str.length -1; i >=0; i--){
-        reversedStr += str[i];
+//     let reversedStr = '';
+//     for (let i = str.length -1; i >=0; i--){
+//         reversedStr += str[i];
+//     }
+
+//     return parseInt(reversedStr);
+// }
+
+// console.log(reversing(12345))
+
+const reversing = (str)=>{
+    let reversed = '';
+
+    for (let i = str.length -1; i >= 0; i--){
+        reversed += str[i]
     }
 
-    return parseInt(reversedStr);
+    return reversed;
 }
 
-console.log(reversing(12345))
+console.log(reversing("faisal"))
